dbb01e88fd108daeb39b5da9cc7593c540d5b17f
AugustasPaliulis <Augustas.paliulis@outlook.com>
2023-10-10 17:46:29 +0200

    basic input design

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Button from "@/components/button";
+import Input from "@/components/input";
 
 export default function Home() {
   return (
     <div>
-      Hello <Button>BUTTON</Button>
+      Hello <Button>BUTTON</Button> <Input placeholder="working" />
     </div>
   );
 }
---
+++ src/components/input/index.js
@@ -0,0 +1,19 @@
+import styles from "./Input.module.scss";
+import { Inter } from "next/font/google";
+
+const Input = ({ placeholder, value, type, onChange, label }) => {
+  return (
+    <div className={styles["input-container"]}>
+      <div className={styles.label}>{label}</div>
+      <input
+        className={styles.input}
+        placeholder={placeholder}
+        value={value}
+        type={type}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default Input;
