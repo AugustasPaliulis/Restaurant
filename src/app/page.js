a035b235643d220fe9ec3c1f0a81c577e7b28775
AugustasPaliulis <Augustas.paliulis@outlook.com>
2023-10-17 17:11:17 +0200

    about

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import InputButton from "@/components/input_button";
 import Hero from "@/components/page_parts/hero";
+import HomeAbout from "@/components/page_parts/home_about";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <HomeAbout />
     </div>
   );
 }
