3132e220b97aca3020bf62683edb9c1cc69cbdd7
AugustasPaliulis <Augustas.paliulis@outlook.com>
2023-10-17 09:31:47 +0200

    navbar

---
+++ src/components/navbar/index.js
@@ -0,0 +1,91 @@
+"use client";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+import styles from "./Navbar.module.scss";
+import search from "../../icons/search.svg";
+import person from "../../icons/person.svg";
+import cart from "../../icons/cart.svg";
+import hamburger from "../../icons/hamburger.svg";
+import cross from "../../icons/cross.svg";
+import { useState } from "react";
+
+const Navbar = () => {
+  const pathname = usePathname();
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
+  return (
+    <div className={styles.navbarContainer}>
+      <div className={styles.logoContainer}>
+        <Link href="/">
+          F<span className={styles.logoColoredLetters}>oo</span>dtuck
+        </Link>
+      </div>
+      <div className={styles.linkingContainer}>
+        {/* HAMBURGER */}
+
+        <ul className={navbarOpen ? styles.show : null}>
+          <li className={pathname === "/" ? styles.selected : ""}>
+            <Link onClick={() => closeMenu()} href="/">
+              Home
+            </Link>
+          </li>
+          <li className={pathname === "/menu" ? styles.selected : ""}>
+            <Link onClick={() => closeMenu()} href="/">
+              Menu
+            </Link>
+          </li>
+          <li className={pathname === "/about" ? styles.selected : ""}>
+            <Link onClick={() => closeMenu()} href="/">
+              About
+            </Link>
+          </li>
+          <li className={pathname === "/shop" ? styles.selected : ""}>
+            <Link onClick={() => closeMenu()} href="/">
+              Shop
+            </Link>
+          </li>
+          <li className={pathname === "/contact" ? styles.selected : ""}>
+            <Link onClick={() => closeMenu()} href="/">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.iconsContainer}>
+        <ul>
+          <li>
+            <Image src={search} alt="search" width="20" height="auto" />
+          </li>
+          <li>
+            <Image src={person} alt="person" width="20" height="auto" />
+          </li>
+          <li>
+            <Image src={cart} alt="cart" width="20" height="auto" />
+          </li>
+        </ul>
+      </div>
+      <button
+        className={`${styles.desktopHidden} ${styles.hamburger}`}
+        onClick={handleToggle}
+      >
+        {!navbarOpen ? (
+          <Image src={hamburger} alt="hamburger" />
+        ) : (
+          <Image src={cross} alt="cross" />
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
