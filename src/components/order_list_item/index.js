92b62a85d4705b17084691dcd13ef867529ba335
AugustasPaliulis <augustas.paliulis@outlook.com>
2024-01-30 17:29:03 +0100

    possibility tp change quantity in checkout

---
+++ src/components/order_list_item/index.js
@@ -0,0 +1,93 @@
+"use client";
+import { useState, useContext, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { FirebaseAuthUser } from "@/context/firebase/auth/context";
+import styles from "./OrderItem.module.scss";
+
+import Plus from "@/icons/plus";
+import Minus from "@/icons/minus";
+import Trash from "@/icons/trash";
+import { saveToSessionStorage } from "@/context/session/cart_session";
+
+const OrderListItem = ({ index, mealName, quantity, price }) => {
+  const user = useContext(FirebaseAuthUser); // Getting user context
+  const [itemQuantity, setItemQuantity] = useState(quantity);
+  const [itemPressed, setItemPressed] = useState(false);
+
+  // Changin quantity of item in global state
+  useEffect(() => {
+    const itemIndex = user.cart.findIndex((item) => item.mealName === mealName);
+    if (itemIndex !== -1) {
+      const updatedItems = [...user.cart];
+      if (itemQuantity === 0) {
+        // Remove item from array
+        updatedItems.splice(itemIndex, 1);
+      } else {
+        updatedItems[itemIndex].quantity = itemQuantity;
+        updatedItems[itemIndex].price = +(
+          itemQuantity *
+          (price / quantity)
+        ).toFixed(2);
+      }
+      user.setCart(updatedItems);
+      saveToSessionStorage("cart", updatedItems);
+    }
+  }, [itemQuantity]);
+
+  // Increase/decrease quantity functions
+  const increaseQuantity = (event) => {
+    event.stopPropagation();
+    setItemQuantity(itemQuantity + 1);
+  };
+  const decreaseQuantity = (event) => {
+    event.stopPropagation();
+    if (itemQuantity !== 0) {
+      setItemQuantity(itemQuantity - 1);
+    }
+  };
+
+  const itemPressedHandler = () => {
+    setItemPressed(!itemPressed);
+  };
+
+  return (
+    <>
+      {itemPressed ? (
+        <li
+          onClick={itemPressedHandler}
+          className={`${styles.item} ${styles.pressedItem}`}
+        >
+          <div className={styles.itemName}>{mealName}</div>
+
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -100 }}
+            className={styles.quantityChangingGroup}
+          >
+            <div className={styles.quantity}>x{itemQuantity}</div>
+            <Minus
+              onClick={(e) => decreaseQuantity(e)}
+              width={15}
+              height={15}
+            />
+            <Plus onClick={increaseQuantity} width={15} height={15} />
+            <Trash />
+          </motion.div>
+          <div className={styles.price}>{price}</div>
+        </li>
+      ) : (
+        <li onClick={itemPressedHandler} className={styles.item} key={index}>
+          <div className={styles.titleQuantityGroup}>
+            <div>{mealName}</div>
+            <div className={styles.quantity}>x{itemQuantity}</div>
+          </div>
+          <div className={styles.price}>{price}</div>
+        </li>
+      )}
+    </>
+  );
+};
+
+export default OrderListItem;
--- src/components/page_parts/complete_order_list/index.js
+++ src/components/page_parts/complete_order_list/index.js
@@ -3,22 +3,37 @@ import { useContext } from "react";
 import { FirebaseAuthUser } from "@/context/firebase/auth/context";
 
 import styles from "./OrderList.module.scss";
+import Link from "next/link";
+import OrderListItem from "@/components/order_list_item";
 
 const CompleteOrderList = () => {
   const user = useContext(FirebaseAuthUser); // user context
-  if (user && user.cart) {
+  if (user && user.cart && user.cart.length > 0) {
     const cartItems = user.cart.map((item, index) => (
-      <li key={index}>
-        <div className={styles.titleQuantityGroup}>
-          <div>{item.mealName}</div>
-          <div className={styles.quantity}>x{item.quantity}</div>
-        </div>
-        <div className={styles.price}>{item.price}</div>
-      </li>
+      <OrderListItem
+        index={index}
+        mealName={item.mealName}
+        quantity={item.quantity}
+        price={item.price}
+      />
+      // <li key={index}>
+      //   <div className={styles.titleQuantityGroup}>
+      //     <div>{item.mealName}</div>
+      //     <div className={styles.quantity}>x{item.quantity}</div>
+      //   </div>
+      //   <div className={styles.price}>{item.price}</div>
+      // </li>
     ));
     return <ul className={styles.listContainer}>{cartItems}</ul>;
   }
-  return <div>No items in cart</div>;
+  return (
+    <div className={styles.listContainer}>
+      No items in cart
+      <div className={styles.orderLink}>
+        Please order <Link href="/menu">here</Link>
+      </div>
+    </div>
+  );
 };
 
 export default CompleteOrderList;
