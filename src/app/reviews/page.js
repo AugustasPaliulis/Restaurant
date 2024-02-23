6206a3a2c2147ae68231d747f4cffd50db2193f5
AugustasPaliulis <augustas.paliulis@outlook.com>
2024-02-23 18:52:28 +0100

    more layout changes

--- src/app/reviews/page.js
+++ src/app/reviews/page.js
@@ -1,9 +1,20 @@
+import Link from "next/link";
+import styles from "./Reviews.module.scss";
 import ReviewsList from "@/components/page_parts/reviews_list";
 
 const ReviewsPage = () => {
   return (
     <div>
-      <h1>Reviews</h1>
+      <div className={styles.reviewHeroContainer}>
+        <div className={styles.heroTextContainer}>
+          <h1 className={styles.title}>Our reviews</h1>
+          <div className={styles.navigation}>
+            <Link href="/">Home </Link>
+            &gt;
+            <Link href="/reviews">Reviews</Link>
+          </div>
+        </div>
+      </div>
       <ReviewsList />
     </div>
   );
